1a0114525751de0a4eae4a0ca01d5175998e3d48
EdsonCJr <ecavicchioj@gmail.com>
2017-04-18 13:19:29 -0300

    Teste
    Teste de craição de gráfico com Gchart

--- AcompanhamentoFila/src/main/java/br/com/acompanhamentofila/bean/ChamadoGraficoBean.java
+++ AcompanhamentoFila/src/main/java/br/com/acompanhamentofila/bean/ChamadoGraficoBean.java
@@ -3,7 +3,6 @@ package br.com.acompanhamentofila.bean;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
@@ -11,6 +10,9 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.omnifaces.util.Messages;
+import org.primefaces.extensions.component.gchart.model.GChartModel;
+import org.primefaces.extensions.component.gchart.model.GChartModelBuilder;
+import org.primefaces.extensions.component.gchart.model.GChartType;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
@@ -29,75 +31,81 @@ public class ChamadoGraficoBean {
 		return barModel;
 	}
 
+	private GChartModel chartModel;
+
+	public GChartModel getChartModel() {
+		return chartModel;
+	}
+
 	@PostConstruct
 	public void init() {
 		createBarModels();
 	}
 
 	private BarChartModel initBarModel() {
-		
+
 		ChamadoDAO chamadoDAO = new ChamadoDAO();
 		List<Chamado> listaChamadosAbertos = new ArrayList<>();
-		
-		try{
+
+		try {
 			listaChamadosAbertos = chamadoDAO.listarChamadosAbertos();
-		} catch(RuntimeException exception){
+		} catch (RuntimeException exception) {
 			Messages.addGlobalError("Ocorreu um erro ao gerar a lista de chamados");
 			exception.printStackTrace();
 		}
-		
+
 		int chAvencer = 0;
 		int chHoje = 0;
 		int chAmanha = 0;
 		int chVencido = 0;
-		
+
 		DateTime dataAtual = new DateTime();
 		DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy");
-		
-		for(Chamado c : listaChamadosAbertos){
-			
+
+		for (Chamado c : listaChamadosAbertos) {
+
 			DateTime slaChamado = new DateTime(c.getVencimentoSla());
-			
-			if(!slaChamado.isEqual(null)){
-				
+
+			if (!slaChamado.isEqual(null)) {
+
 				String SslaCh = slaChamado.toString(fmt);
 				DateTime sla = fmt.parseDateTime(SslaCh);
-				
+
 				String SdtAtual = dataAtual.toString(fmt);
 				DateTime dtAtual = fmt.parseDateTime(SdtAtual);
-				
-				if(sla.isBefore(dtAtual)){
+
+				if (sla.isBefore(dtAtual)) {
 					chVencido += 1;
 				}
-				
-				if(sla.isEqual(dtAtual)){
+
+				if (sla.isEqual(dtAtual)) {
 					chHoje += 1;
 				}
-				
-				if(sla.isEqual(dtAtual.plusDays(1))){
+
+				if (sla.isEqual(dtAtual.plusDays(1))) {
 					chAmanha += 1;
 				}
-				
-				if (sla.isAfter(dtAtual.plusDays(1))){
+
+				if (sla.isAfter(dtAtual.plusDays(1))) {
 					chAvencer += 1;
 				}
 			}
 		}
-				
+
 		BarChartModel model = new BarChartModel();
-		
+
 		ChartSeries vencido = new ChartSeries();
 		vencido.setLabel("Vencidos");
-		vencido.set("Categorias",chVencido);
+		vencido.set("Categorias", chVencido);
 
 		ChartSeries hoje = new ChartSeries();
 		hoje.setLabel("Hoje");
 		hoje.set("", chHoje);
-		
+
 		ChartSeries amanha = new ChartSeries();
 		amanha.setLabel("Amanhã");
 		amanha.set("", chAmanha);
-		
+
 		ChartSeries aVencer = new ChartSeries();
 		aVencer.setLabel("A vencer");
 		aVencer.set("", chAvencer);
@@ -106,12 +114,13 @@ public class ChamadoGraficoBean {
 		model.addSeries(hoje);
 		model.addSeries(amanha);
 		model.addSeries(aVencer);
-				
+
 		return model;
 	}
 
 	private void createBarModels() {
 		createBarModel();
+		createGbarModel();
 	}
 
 	private void createBarModel() {
@@ -119,7 +128,7 @@ public class ChamadoGraficoBean {
 
 		barModel.setTitle("Chamado em atendimento por vencimento");
 		barModel.setLegendPosition("s");
-		//Posição da legenda -> n, s, w, e, se, sw, ne, nw
+		// Posição da legenda -> n, s, w, e, se, sw, ne, nw
 		barModel.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
 		barModel.setLegendCols(4);
 
@@ -131,6 +140,61 @@ public class ChamadoGraficoBean {
 		yAxis.setMin(0);
 		yAxis.setMax(200);
 
+	}
+
+	public void createGbarModel() {
+
+		ChamadoDAO chamadoDAO = new ChamadoDAO();
+		List<Chamado> listaChAbertos = new ArrayList<>();
+
+		try {
+			listaChAbertos = chamadoDAO.listarChamadosAbertos();
+		} catch (RuntimeException exception) {
+			Messages.addGlobalError("Ocorreu um erro ao gerar a lista de chamados");
+			exception.printStackTrace();
+		}
+
+		int chAvencer = 0;
+		int chHoje = 0;
+		int chAmanha = 0;
+		int chVencido = 0;
+
+		DateTime dataAtual = new DateTime();
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy");
+
+		for (Chamado c : listaChAbertos) {
+
+			DateTime slaChamado = new DateTime(c.getVencimentoSla());
+
+			if (!slaChamado.isEqual(null)) {
+
+				String SslaCh = slaChamado.toString(fmt);
+				DateTime sla = fmt.parseDateTime(SslaCh);
+
+				String SdtAtual = dataAtual.toString(fmt);
+				DateTime dtAtual = fmt.parseDateTime(SdtAtual);
+
+				if (sla.isBefore(dtAtual)) {
+					chVencido += 1;
+				}
+
+				if (sla.isEqual(dtAtual)) {
+					chHoje += 1;
+				}
+
+				if (sla.isEqual(dtAtual.plusDays(1))) {
+					chAmanha += 1;
+				}
+
+				if (sla.isAfter(dtAtual.plusDays(1))) {
+					chAvencer += 1;
+				}
+			}
+		}
+
+		chartModel = new GChartModelBuilder().setChartType(GChartType.COLUMN)
+				.addColumns("Vencido", "Hoje", "Amanhã", "A vencer").addRow("Vencido", chVencido).addRow("Hoje", chHoje)
+				.addRow("Amanhã", chAmanha).addRow("A vencer", chAvencer).build();
 	}
 
 }
